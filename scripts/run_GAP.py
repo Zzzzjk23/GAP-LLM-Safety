254fc73fef13a195ff0f1786d0af131349b38d02
Zzzzjk23 <95385988+Zzzzjk23@users.noreply.github.com>
2026-01-30 03:22:48 +0800

    Use Nebius Token Factory models in scripts

--- scripts/run_GAP.py
+++ scripts/run_GAP.py
@@ -1,38 +1,32 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
 import sys
+
 from gap_easyjailbreak.attacker.GAP_Schwartz_2024 import GAP
 
 from easyjailbreak.datasets import JailbreakDataset
-from easyjailbreak.models.huggingface_model import from_pretrained
 from easyjailbreak.models.openai_model import OpenaiModel
-from easyjailbreak.models.huggingface_model import HuggingfaceModel
-from easyjailbreak.models.openai_model import OpenaiModel
-from transformers import AutoTokenizer, AutoModelForCausalLM
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+os.environ["OPENAI_BASE_URL"] = "https://api.tokenfactory.nebius.com/v1/"
 
 sys.path.append(os.getcwd())
 dataset_name_or_path = '../easyjailbreak/datasets/data/advbench_subset.json'
 dataset = JailbreakDataset(local_file_type='json', dataset=dataset_name_or_path)
 
-openai_key = os.environ["OPENAI_KEY"]
+openai_key = "v1.CmQKHHN0YXRpY2tleS1lMDBxMDN4MHl3d3FlZ2Q4cDkSIXNlcnZpY2VhY2NvdW50LWUwMHdwcWpmYWcwNXp6YTRrZzIMCMrxgcoGEPyfrr0DOgwIy_SZlQcQgIyNngJAAloDZTAw.AAAAAAAAAAFZJjga8wP8uscjRXekw5wFdE2ySYvpwKLD-pH8KlRyKUYypqrhqdunKX2HlZ7svbE61JCwPV0gTI3eXy-3Z7AG"
 
-# attack_model = OpenaiModel(model_name=attack_model_name, api_keys=openai_key)
-# attack_model = MistralModel(model_name="mistral.mistral-large-2407-v1:0")
-attack_model_name = 'vicuna_v1.1'
-attack_model_path = 'lmsys/vicuna-13b-v1.5'
-attack_model = from_pretrained(attack_model_path, model_name=attack_model_name)
-
-
-eval_model_name = 'gpt-4'
-eval_model = OpenaiModel(model_name=eval_model_name, api_keys=openai_key)
-
-# tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
-# model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.1-8B-Instruct", device_map='auto')
-# target_model = HuggingfaceModel(model_name='llama-3', model=model, tokenizer=tokenizer)
-# tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
-# model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", device_map='auto')
-# target_model = HuggingfaceModel(model_name='llama-2', model=model, tokenizer=tokenizer)
-target_model = OpenaiModel(model_name='gpt-3.5-turbo', api_keys=openai_key)
+attack_model = OpenaiModel(
+    model_name="Qwen/Qwen3-Coder-480B-A35B-Instruct",
+    api_keys=openai_key,
+)
+eval_model = OpenaiModel(
+    model_name="moonshotai/Kimi-K2-Instruct",
+    api_keys=openai_key,
+)
+target_model = OpenaiModel(
+    model_name="openai/gpt-oss-20b",
+    api_keys=openai_key,
+)
 
 dataset_name = 'GAP_Atk_Vic_Evl_GPT4_Tgt_GPT35_D_AdvBench_all'
 attacker = GAP(attack_model=attack_model,
